311c2d03f96f4d9d170c66b598a3fd264ad0daa6
pinkfloyd92 <ascacere92@gmail.com>
2017-06-11 15:42:26 -0500

    irony setup

--- Custom/cmode.el
+++ Custom/cmode.el
@@ -4,15 +4,9 @@
   :defer t
   :init
   (add-hook 'c++-mode-hook 'irony-mode)
-;;  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
   (add-hook 'objc-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook '(lambda ()
-        (setq ac-sources (append '(ac-source-semantic) ac-sources))
-        (local-set-key (kbd "RET") 'newline-and-indent)
-        (linum-mode t)
-        (semantic-mode t)))
   :config
-
   ;; replace the `completion-at-point' and `complete-symbol' bindings in
   ;; irony-mode's buffers by irony-mode's function
   (defun my-irony-mode-hook ()
@@ -42,19 +36,6 @@
 
 )
 
-;; == company-mode ==
-(use-package cedet
-  :ensure t
-  :defer t
-  :config
-(global-ede-mode 1)                      ; Enable the Project management system
-(require 'ede)
-(global-semantic-idle-completions-mode t)
-(global-semantic-decoration-mode t)
-(global-semantic-highlight-func-mode t)
-(global-semantic-show-unmatched-syntax-mode t)
-)
-
 (require 'company-c-headers)
 (add-to-list 'company-backends 'company-c-headers)
-(add-to-list 'company-c-headers-path-system "/lib/modules/4.11.3-1-ARCH/build/include")
+(add-to-list 'company-c-headers-path-system "/lib/modules/4.11.3-1-ARCH/build/include/linux")
